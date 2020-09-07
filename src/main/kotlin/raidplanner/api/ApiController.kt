9ec17ee0f3310eb40d55ba7b3f2324dc65aa95b9
Magnus Mickelsson <magnus.mickelsson@gmail.com>
2020-09-07 11:40:11 +0200

    New endpoints according to UI requirements

--- src/main/kotlin/raidplanner/api/ApiController.kt
+++ src/main/kotlin/raidplanner/api/ApiController.kt
@@ -1,10 +1,10 @@
 package raidplanner.api
 
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import raidplanner.domain.ClassSpec
+import raidplanner.domain.Debuff
 import raidplanner.domain.DomainRepository
+import raidplanner.domain.WowClassName
 
 @CrossOrigin
 @RestController
@@ -13,12 +13,44 @@ class ApiController(private val repository: DomainRepository) {
     @GetMapping("item")
     fun getItems() = repository.getDebuffItems()
 
+    @GetMapping("faction/{faction}/spec")
+    fun getClassSpecsForFaction(@PathVariable("faction") faction: Faction): List<ClassSpec> =
+            filterByFaction(faction, repository.getSpecs())
+
+    @GetMapping("faction/{faction}/specByClass")
+    fun getClassSpecsByClassForFaction(@PathVariable("faction") faction: Faction):
+            Map<WowClassName, List<ClassSpec>> {
+        val map = mutableMapOf<WowClassName, List<ClassSpec>>()
+        filterByFaction(faction, repository.getSpecs()).associateTo(map, {
+            Pair(it.className, (map[it.className] ?: emptyList()).plus(it))
+        })
+        return map.toMap()
+    }
+
+    @GetMapping("faction/{faction}/specByClass/class/{wowClass}")
+    fun getClassSpecsByClassForFaction(@PathVariable("faction") faction: Faction,
+    @PathVariable("wowClass") wowClassName: WowClassName): List<ClassSpec> =
+            getClassSpecsByClassForFaction(faction)[wowClassName] ?: error("No results for $wowClassName")
+
     @GetMapping("spec")
     fun getClassSpecs() = repository.getSpecs()
 
-    @GetMapping("debuff")
-    fun getAllDebuffs() = repository.getSpecs().flatMap { it.canApplyDebuff }.distinct()
+    @GetMapping("faction/{faction}/debuff")
+    fun getDebuffsForFaction(@PathVariable("faction") faction: Faction): List<Debuff> =
+            filterByFaction(faction, repository.getSpecs()).flatMap { it.canApplyDebuff }.distinct()
 
-    @GetMapping("debuffTemplate/aq40")
-    fun getDebuffTemplate() = repository.getDebuffPriorityTemplateAQ40()
+    @GetMapping("debuff")
+    fun getAllDebuffs(): List<Debuff> = repository.getSpecs().flatMap { it.canApplyDebuff }.distinct()
+
+    @GetMapping("faction/{faction}/debuffTemplate/aq40") // Same for both factions at first
+    fun getDebuffTemplate(@PathVariable("faction") faction: Faction) = repository.getDebuffPriorityTemplateAQ40()
+
+    private fun filterByFaction(faction: Faction, specs: List<ClassSpec>): List<ClassSpec> = when (faction) {
+        Faction.alliance -> specs.filter { it.className != WowClassName.SHAMAN }
+        Faction.horde -> specs.filter { it.className != WowClassName.PALADIN }
+    }
+}
+
+enum class Faction {
+    horde, alliance
 }
--- src/test/kotlin/raidplanner/RaidplannerApplicationTests.kt
+++ src/test/kotlin/raidplanner/RaidplannerApplicationTests.kt
@@ -9,5 +9,4 @@ class RaidplannerApplicationTests {
 	@Test
 	fun contextLoads() {
 	}
-
 }
