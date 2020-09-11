250e49911748bf3787ec36b5b6814b47a0c45863
Jesper Hansson Falkenby <jesper.hansson.f@gmail.com>
2020-09-11 19:49:32 +0200

    Update dependencies and fix build
    - Also updated README
    - And added Prettier as a git pre-hook

--- src/main/web/gatsby-ssr.js
+++ src/main/web/gatsby-ssr.js
@@ -5,3 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+import "./src/styles/global.css"
+
+import React from "react"
+import { Provider } from "react-redux"
+import store from "./src/state/store"
+
+export const wrapRootElement = ({ element }) => {
+  return <Provider store={store}>{element}</Provider>
+}
