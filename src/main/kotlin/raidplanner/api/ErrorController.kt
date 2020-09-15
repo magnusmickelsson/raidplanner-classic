676c30516003543d9a32ad688034744b26b91a74
Magnus Mickelsson <magnus.mickelsson@gmail.com>
2020-09-16 01:02:40 +0200

    Try and find issue with heroku deploy, error handling with Thymeleaf.

--- src/main/kotlin/raidplanner/api/ErrorController.kt
+++ src/main/kotlin/raidplanner/api/ErrorController.kt
@@ -1,10 +1,10 @@
 package raidplanner.api
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class ErrorController {
     @GetMapping("/error")
-    fun error() = "{An error occurred. Check logs.}"
+    fun error() = "error"
 }
